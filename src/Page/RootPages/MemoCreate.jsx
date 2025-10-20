07c8c28e8111e808e458ea93c4939643b4fa4673
stanstanstanlee <stanstanstanlee@gmail.com>
2025-10-20 16:10:11 +0900

    memo-ai

--- src/Page/RootPages/MemoCreate.jsx
+++ src/Page/RootPages/MemoCreate.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import { useState } from "react";
+import MessageList from "../../components/MessageList";
+import ChatForm from "../../components/ChatForm";
+import { chat, config } from "../../utils/genai";
 
 export default function MemoCreate() {
   return <div>MemoCreate</div>;
